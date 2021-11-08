c9b65e8f3f2a2a5bfa7ef1ad70b7c1ff49314545
balladyna <winconek.olga@gmail.com>
2021-11-08 23:40:06 +0100

    My solutions of exercises 05-06.

---
+++ ex_05.py
@@ -0,0 +1,15 @@
+his_name = 'Gremlin'
+his_age = 99
+his_height = 48 # cm
+his_weight = 7 # kg
+his_eyes = 'Green'
+his_teeth = 'Yellow'
+his_fur = 'Orange'
+
+print(f"Let's talk about {his_name}.")
+print(f"He's {his_height} cm tall.")
+print(f"He's {his_weight} kg heavy.")
+print(f"Actually that's not a lot.")
+print(f"He's got {his_eyes} eyes and {his_fur} fur.")
+print(f"His teeth are usually {his_teeth} depending on the breakfast.")
+print(f"If I add {his_age}, {his_height} and {his_weight} I get ", his_age + his_height + his_weight ,".")
---
+++ ex_06.py
@@ -0,0 +1,21 @@
+types_of_people = 77
+x = f"There are {types_of_people} types of people."
+
+program = "excel"
+do_not = "don't"
+y = f"Those who know {program} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = True
+joke_evaluation = "Isn't this joke not so funny? {}"
+print(joke_evaluation.format(hilarious))
+
+a = "This is the left side of ..."
+b = "a string with a right side."
+
+print(a + b)
