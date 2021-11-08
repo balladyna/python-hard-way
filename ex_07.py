90909118901e2e63d463c0ac7872f5f26bd7e027
balladyna <winconek.olga@gmail.com>
2021-11-08 23:55:27 +0100

    My solutions of exercises 07.

---
+++ ex_07.py
@@ -0,0 +1,17 @@
+print("Mary had a little Pterodactylus.")
+print("Its skin was  white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10)
+
+end1 = "B"
+end2 = "e"
+end3 = "t"
+end4 = "e"
+end5 = "l"
+end6 = "g"
+end7 = "e"
+end8 = "u"
+end9 = "s"
+end10 = "e"
+
+print(end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10)
